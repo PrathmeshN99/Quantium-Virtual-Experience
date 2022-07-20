588c8a6732a2addc3e80f0226b72c29a90f3f864
PrathmeshN99 <90515944+PrathmeshN99@users.noreply.github.com>
2022-07-20 14:52:22 +0530

    t

--- Quantium-Virtual-Experience/test_dashAppEnhanced.py
+++ Quantium-Virtual-Experience/test_dashAppEnhanced.py
@@ -1,16 +1,16 @@
-import unittest
-import dashAppEnchanced
-import plotly.express as px  # (version 4.7.0 or higher)
-from dash import Dash, dcc, html, Input, Output  # pip install dash (version 2.0.0 or higher)
+from pink_morsel_visualizer import dash_app
 
-class TestApp(unittest.TestCase):
 
-    def test_dashAppEnhanced(dashAppEnhanced):
-        app = Dash(__name__)
-        app.layout = html.Div("Sales of Pink Morsels by region and date", style={'text-align': 'center'},id="h1_text")
-    
-        dashAppEnchanced.start_server(app)
+def test_header_exists(dash_duo):
+    dash_duo.start_server(dash_app)
+    dash_duo.wait_for_element("#header", timeout=10)
 
-        dashAppEnchanced.wait_for_contains_text("#h1_test", "Sales of Pink Morsels by region and date", timeout=None)
-        dashAppEnhanced.wait_for_element_by_id("selectRegion", timeout=None)
-        dashAppEnhanced.wait_for_element_by_id("my_bee_map", timeout=None)
+
+def test_visualization_exists(dash_duo):
+    dash_duo.start_server(dash_app)
+    dash_duo.wait_for_element("#visualization", timeout=10)
+
+
+def test_region_picker_exists(dash_duo):
+    dash_duo.start_server(dash_app)
+    dash_duo.wait_for_element("#region_picker", timeout=10)
